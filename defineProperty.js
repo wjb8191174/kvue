887279a346ddf8c71722bb389e8c3e120839aa9c
wangjiab <17600275226@163.com>
2021-04-12 23:02:03 +0800

    commint

--- defineProperty.js
+++ defineProperty.js
@@ -1,6 +1,9 @@
 // 数据响应式原理
 // Object.defineProperty
 function defineReaction(obj, key, val) {
+    // val 可能是对象 需要递归调用
+    observer(val)
+
     Object.defineProperty(obj, key, 
         {
             get() {
@@ -10,6 +13,8 @@ function defineReaction(obj, key, val) {
             set(newVal) {
                 if(newVal !== val) {
                     console.log('set', newVal);
+                    // newVal 可能对象 需要递归调用
+                    observer(newVal)
                     val = newVal
                 }
             }
@@ -17,9 +22,36 @@ function defineReaction(obj, key, val) {
     )
 }
 
-var obj = {}
+function set(obj, key, val) {
+    defineReaction(obj, key, val)
+}
 
-defineReaction(obj, 'foo', 'foo')
+// 对象响应式处理
+function observer(obj) {
+    if(typeof obj !== 'object' || obj == null) {
+        return
+    }
+    Object.keys(obj).forEach(key => defineReaction(obj, key, obj[key]))
+}
 
-obj.foo
-obj.foo = 'foooo'
+var obj = {foo: 'foo', baz: 'baz', bar: {a: 1}}
+
+// defineReaction(obj, 'foo', 'foo')
+observer(obj)
+
+// obj.foo
+// obj.foo = 'foooo'
+
+// obj.baz
+
+// bar 是对象
+// obj.bar.a = 10
+
+// newVal 是对象
+// obj.bar = {a : 10}
+// obj.bar.a = 100
+
+// 动态赋值
+set(obj, 'dong', 'dong')
+obj.dong
+obj.dong = 'dongdong'
